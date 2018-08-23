7a0295467404a3ee44c3fd51022865728232668a
John Zila <john@keyba.se>
2018-08-22 17:46:54 -0700

    fix

--- shared/actions/chat2/index.js
+++ shared/actions/chat2/index.js
@@ -2329,7 +2329,7 @@ const setMinWriterRole = (action: Chat2Gen.SetMinWriterRolePayload) => {
   })
 }
 
-const openChatFromWidget = (state: TypedState, {payload: {conversationIDKey}}: Chat2Gen.OpenChatFromWidget) =>
+const openChatFromWidget = (state: TypedState, {payload: {conversationIDKey}}: Chat2Gen.OpenChatFromWidgetPayload) =>
   Saga.sequentially([
     Saga.put(ConfigGen.createShowMain()),
     Saga.put(switchTo([chatTab])),
